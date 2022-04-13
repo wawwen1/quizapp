70b447e75244b6500a8933cb11db807e09a5a5be
warren roque <warrenroque17@gmail.com>
2022-04-13 20:00:09 +0000

    edit tables + formatting

--- db/schema/02_tables.sql
+++ db/schema/02_tables.sql
@@ -12,19 +12,21 @@ CREATE TABLE users (
 CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  listed BOOLEAN NOT NULL,
-  created DATE NOT NULL,
   description TEXT NOT NULL,
-  created_by INT REFERENCES users(id) ON DELETE CASCADE
+  private BOOLEAN NOT NULL DEFAULT FALSE,
+  created DATE NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
+
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INT REFERENCES quizzes(id) ON DELETE CASCADE,
-  question VARCHAR(510) NOT NULL
+  question VARCHAR(510) NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE
 );
+
 CREATE TABLE answers (
   id SERIAL PRIMARY KEY NOT NULL,
   answer TEXT NOT NULL,
-  question_id INT REFERENCES questions(id) ON DELETE CASCADE,
-  correct BOOLEAN
+  correct BOOLEAN DEFAULT FALSE,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE
 );
