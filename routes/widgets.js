9d943cf30d6d8e27d7dab1b600e4e418b15dd8a0
warren roque <warrenroque17@gmail.com>
2022-04-14 23:40:04 +0000

    fix code to store correct/incorrect values properly

--- routes/widgets.js
+++ routes/widgets.js
@@ -37,8 +37,8 @@ module.exports = (db) => {
     const q2 = { question: req.body.q2, input: req.body.input2, answer: req.body.answer2 };
     const q3 = { question: req.body.q3, input: req.body.input3, answer: req.body.answer3 };
 
-    const newQuiz = { q1, q2, q3 };
-
+    const newQuiz = [ q1, q2, q3 ];
+    console.log(q1);
     if (private === undefined) {
       private = false;
     }
@@ -47,51 +47,69 @@ module.exports = (db) => {
       return res.send("Please fill out the quiz completely! Go back");
     }
 
+    //store quiz description
     db.query(`
     INSERT INTO quizzes (owner_id, name, description, private)
     VALUES ($1, $2, $3, $4)
     RETURNING *;
     `, [req.params.id, name, description, private])
       .then(res => {
-        let quizID = res.rows[0].id;    //take quiz.id to input into questions
+      let quizID = res.rows[0].id;
+      console.log(quizID);
 
-//LOOP THROUGH NEWQUIZ OBJECT TO ADD INDIVIDUAL QUESTIONS TO TABLE
-        for (let question of Object.keys(newQuiz)) {
-          db.query(`
-        INSERT INTO questions (quiz_id, question)
-        VALUES ($1, $2)
-        RETURNING *;
-        `, [quizID, newQuiz[question].question])
+      //store question details
+     for (const quiz of newQuiz) {
+       console.log(quiz);
+       db.query(`
+       INSERT INTO questions (quiz_id, question)
+       VALUES ($1, $2)
+       RETURNING *;
+       `, [quizID, quiz.question])
+       .then(res => {
+        let questionID = res.rows[0].id;
+         console.log('QUESTION ID IS EQUAL TO: ', questionID);
+         for (let i = 1; i <= quiz.input.length; i++) {
+
+          //store correct answers
+           if (i === parseInt(quiz.answer)) {
+             db.query(`
+             INSERT INTO answers (question_id, answer, correct)
+             VALUES ($1, $2, $3)
+             RETURNING *;
+             `, [questionID, quiz.input[i-1], true])
+             .then(res => {
+              console.log('STORING CORRECT ANSWER TO DB: ', quiz.input[i-1]);
+             })
+             .catch(err => {
+               console.log(err);
+             })
+
+          //store wrong answers
+           } else {
+            db.query(`
+            INSERT INTO answers (question_id, answer, correct)
+            VALUES ($1, $2, $3)
+            RETURNING *;
+            `, [questionID, quiz.input[i-1], false])
             .then(res => {
-              console.log(res.rows[0]);
-              let questionID = res.rows[0].id;    //take question.id input into answers
-
-//WHEN RADIO VALUE EQUALS ANSWER VALUE -> RESULT = TRUE
-
-               // console.log(newQuiz[question].input);
-              for (let input of newQuiz[question].input) {
-
-                let correct = (newQuiz[question].answer ? true : false);  //true to match boolean input
-                db.query(`
-                INSERT INTO answers (question_id, answer, correct)
-                VALUES ($1, $2, $3)
-                RETURNING *;
-                `, [questionID, input, correct])
-                .then(res => {
-                  console.log(res.rows[0]);
-                })
-                .catch(err => {
-                  res
-                  .status(500)
-                  .json({ error: err.message });
-                });
-              }
-            });
+             console.log('STORING WRONG ANSWER TO DB: ', quiz.input[i-1]);
+            })
+            .catch(err => {
+              console.log(err);
+            })
+          }
         }
-      });
+      })
+       .catch(err => {
+         console.log(err);
+       })
+     }
+      })
+      .catch(err => {
+        console.log(err);
+    });
+
       return res.redirect("/quizzes");
   });
-
-
   return router;
 };
